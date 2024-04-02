d5993b94dfb5c765e042e8735b713c0dd6047824
张松贵 <82018238@qq.com>
2024-04-02 20:25:58 +0800

    删除错误上传的配置文件

--- comtradeParser/computation/impedance.py
+++ comtradeParser/computation/impedance.py
@@ -30,7 +30,7 @@ def compute_line_impedance(this_before_v_xfl: list, this_before_i_xfl: list,
                         this_after_v_xfl[2]) /
                        (this_before_i_xfl[1] * other_after_v_xfl[2] - other_before_v_xfl[1] *
                         this_after_i_xfl[2]))
-    ##（本侧零序电压x对侧负序电压 - 本侧负序电压x对侧零序电压 + 本侧负序电流x对侧零序电压xZ1） /
+    # （本侧零序电压x对侧负序电压 - 本侧负序电压x对侧零序电压 + 本侧负序电流x对侧零序电压xZ1） /
     # （本侧零序电流x对侧负序电压），其中Z1为上次运算的正序阻抗结果
     zero_impedance = (this_after_v_xfl[0] * other_after_v_xfl[2] - this_after_v_xfl[2] * other_after_v_xfl[0] +
                       this_after_i_xfl[2] * other_after_v_xfl[0] * order_impedance) / (
