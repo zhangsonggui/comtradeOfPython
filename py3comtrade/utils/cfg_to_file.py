5182878ed7fa33623a67f2a8d75e41c8e5f1acb9
张松贵 <82018238@qq.com>
2024-07-11 16:24:40 +0800

    cfg对象保存为文件

--- py3comtrade/utils/cfg_to_file.py
+++ py3comtrade/utils/cfg_to_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from py3comtrade.entity.cfg import Cfg
 from py3comtrade.parser.analog_channel import generate_analog_channel_str
-from py3comtrade.parser.cfg_parser import CfgParser
 from py3comtrade.parser.digital_channel import generate_digital_channel_str
 from py3comtrade.parser.fault_header import generate_fault_header_str
 from py3comtrade.parser.sample_info import generate_sample_info_str
@@ -24,12 +24,12 @@ def generate_cfg_str(cfg_obj):
     return fault_headers_str + analog_channels_str + digital_channels_str + sample_info_str
 
 
-def cfg_to_file(cfg: CfgParser, filename: str):
+def cfg_to_file(cfg: Cfg, filename: str):
     """
     将cfg文件写入文件
     :param cfg: cfg文件对象
     :param filename: 文件名
     """
     with open(filename, 'w', encoding='gbk') as f:
-        f.write(generate_cfg_str(cfg.cfg))
+        f.write(generate_cfg_str(cfg))
     return f'{filename}文件生成成功！'
