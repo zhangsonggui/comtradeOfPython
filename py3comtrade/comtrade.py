daa36decac43f835acef9d3c0050631c18cdcb43
张松贵 <82018238@qq.com>
2024-12-08 21:44:32 +0800

    测试增加木兰协议

--- py3comtrade/comtrade.py
+++ py3comtrade/comtrade.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 # _*_ coding: utf-8 _*_
+
+#  Copyright (c) [2019] [name of copyright holder]
+#  [py3comtrade] is licensed under Mulan PSL v2.
+#  You can use this software according to the terms and conditions of the Mulan
+#  PSL v2.
+#  You may obtain a copy of Mulan PSL v2 at:
+#           http://license.coscl.org.cn/MulanPSL2
+#  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+#  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+#  NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#  See the Mulan PSL v2 for more details.
+
 #
-# Copyright (C) 2024 - 2024 张松贵, Inc. All Rights Reserved
 #
 # @Time    : 2024/3/23 11:27
 # @Author  : 张松贵
