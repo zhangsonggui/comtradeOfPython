4396a52db696e03a3d6b99020f607609396c8184
张松贵 <82018238@qq.com>
2025-06-20 22:28:20 +0800

    refactor(dmf_reader): 重构 DMF 解析逻辑
    -将 DMF 解析逻辑拆分为多个独立的函数，提高代码可读性和可维护性
    - 新增 analog_channel_parser、status_channel_parser、acv_branch_parser 等辅助函数，分别解析不同类型的通道和设备
    - 优化了 XML 命名空间的使用，提高了代码的健壮性
    - 删除了未使用的 channel_parser 函数，简化了代码结构

--- py3comtrade/model/transformer.py
+++ py3comtrade/model/transformer.py
@@ -37,8 +37,8 @@ class TransformerWinding(BaseModel):
     bus_id: int = Field(default=0, description="母线索引号")
     location: TransWindLocation = Field(default=TransWindLocation.HIGH, description="绕组位置")
     reference: Optional[str] = Field(default=None, description="IEC61850参引")
-    vrtg: float = Field(default=0.0, description="额定电压")
-    artg: float = Field(default=0.0, description="一次额定电流")
+    v_rtg: float = Field(default=0.0, description="额定电压")
+    a_rtg: float = Field(default=0.0, description="一次额定电流")
     bran_num: int = Field(default=0, description="分路数")
     wg: WG = Field(default=WG(), description="绕组标识符")
     acv_chn: ACVBranch = Field(default=ACVBranch(), description="交流电压通道")
@@ -46,7 +46,7 @@ class TransformerWinding(BaseModel):
     igap: Igap = Field(default=Igap(), description="中性点电流通道号")
 
     def __str__(self):
-        xml = f"<scl:TransformerWinding location={self.location.get_code()} srcRef={self.reference} VRtg={self.vrtg} ARtg={self.artg} bran_num={self.bran_num} bus_ID={self.bus_id} wG="">"
+        xml = f"<scl:TransformerWinding location={self.location.get_code()} srcRef={self.reference} VRtg={self.v_rtg} ARtg={self.a_rtg} bran_num={self.bran_num} bus_ID={self.bus_id} wG="">"
         xml += str(self.acv_chn)
         for acc_bran in self.acc_bran:
             xml += str(acc_bran)
@@ -55,13 +55,14 @@ class TransformerWinding(BaseModel):
 
 
 class Transformer(PrimaryEquipments):
-    pwrRtg: float = Field(default=0.0, description="变压器额定功率")
+    pwr_rtg: float = Field(default=0.0, description="变压器额定功率")
     transWinds: List[TransformerWinding] = Field(default_factory=list, description="变压器绕组")
     ana_chn: List[ChannelIdx] = Field(default_factory=list, description="模拟通道索引号")
     sta_chn: List[ChannelIdx] = Field(default_factory=list, description="开关量通道索引号")
+    transformer_uuid: str = Field(default="", description="变压器标识")
 
     def __str__(self):
-        xml = f"<scl:Transformer idx={self.idx} trm_name={self.name} srcRef={self.reference} pwrRtg={self.pwrRtg} transformer_uuid="">"
+        xml = f"<scl:Transformer idx={self.idx} trm_name={self.name} srcRef={self.reference} pwrRtg={self.pwr_rtg} transformer_uuid={self.transformer_uuid}>"
         for trans_wind in self.transWinds:
             xml += str(trans_wind)
         for ana_chn in self.ana_chn:
--- py3comtrade/reader/dmf_reader.py
+++ py3comtrade/reader/dmf_reader.py
@@ -24,182 +24,190 @@ from py3comtrade.model.type.analog_enum import Multiplier, TvInstallation, BranN
     WGFlag
 
 
-def channel_parser(channel_xml):
-    pass
+def analog_channel_parser(channel_xml) -> AnalogChannel:
+    """
+    解析模拟量通道
+    :param channel_xml: 模拟量通道字符串
+    :return 模拟量通道对象
+    """
+    idx_cfg = channel_xml.get('idx_cfg', None)
+    idx_org = channel_xml.get('idx_org', "")
+    type = channel_xml.get('type', None)
+    flag = channel_xml.get('flag', None)
+    freq = channel_xml.get('freq', 50)
+    au = channel_xml.get('au', 1)
+    bu = channel_xml.get('bu', 0)
+    sIUnit = channel_xml.get('sIUnit', None)
+    multiplier = channel_xml.get('multiplier', "")
+    primary = channel_xml.get('primary', 1)
+    secondary = channel_xml.get('secondary', 1)
+    ps = channel_xml.get('ps', "")
+    ph = channel_xml.get('ph', "")
+    p_min = channel_xml.get('p_min', None)
+    p_max = channel_xml.get('p_max', None)
+    s_min = channel_xml.get('s_min', None)
+    s_max = channel_xml.get('s_max', None)
+    return AnalogChannel(idx_cfg=idx_cfg,
+                         idx_org=idx_org,
+                         type=type,
+                         flag=flag,
+                         freq=freq,
+                         au=au,
+                         bu=bu,
+                         s_i_unit=sIUnit,
+                         multiplier=Multiplier.from_string(multiplier, default=Multiplier.N),
+                         primary=primary,
+                         secondary=secondary,
+                         ps=PsType.from_string(ps, default=PsType.S),
+                         phase=ph,
+                         p_max=p_max,
+                         p_min=p_min,
+                         s_max=s_max,
+                         s_min=s_min)
 
 
-def dmf_parser(file_path) -> DMF:
-    dmf = DMF()
-    tree = ET.parse(file_path)
+def status_channel_parser(channel_xml) -> StatusChannel:
+    """解析开关量通道"""
+    idx_cfg = channel_xml.get('idx_cfg', None)
+    idx_org = channel_xml.get('idx_org', "")
+    signal_type_str = channel_xml.get('type', None)
+    signal_type = SignalType.from_string(signal_type_str, default=SignalType.RELAY)
+    flag = channel_xml.get('flag', ChannelFlag.GENERAL)
+    if signal_type is SignalType.RELAY:
+        flag = RelayFlag.from_string(flag, default=RelayFlag.TR)
+    elif signal_type is SignalType.BREAKER:
+        flag = BreakerFlag.from_string(flag, default=BreakerFlag.HWJ)
+    elif signal_type is SignalType.WARNING:
+        flag = WarningFlag.from_string(flag, default=WarningFlag.WARN_COMM)
+    else:
+        flag = ChannelFlag.GENERAL
+    contact = channel_xml.get('contact', None)
+    srcRef = channel_xml.get('srcRef', "")
+
+    return StatusChannel(idx_cfg=idx_cfg, idx_org=idx_org, type=signal_type, flag=flag, reference=srcRef,
+                         contact=Contact.from_string(contact, default=Contact.NORMALLY_OPEN))
+
+
+def acv_branch_parser(branch_xml) -> ACVBranch:
+    """解析电压分支"""
+    return ACVBranch(ua_idx=branch_xml.get('ua_idx', ""),
+                     ub_idx=branch_xml.get('ub_idx', ""),
+                     uc_idx=branch_xml.get('uc_idx', ""),
+                     ul_idx=branch_xml.get('ul_idx', ""),
+                     un_idx=branch_xml.get('un_idx', ""))
+
+
+def bus_parser(bus_xml, ns) -> Bus:
+    """解析母线"""
+    idx = bus_xml.get('idx', 1)
+    bus_name = bus_xml.get('bus_name', "")
+    src_ref = bus_xml.get('srcRef', "")
+    v_rtg = bus_xml.get('VRtg', None)
+    v_rtg_snd = bus_xml.get('VRtgSnd', None)
+    v_rtg_snd_pos_str = bus_xml.get('VRtgSnd_Pos', "")
+    v_rtg_snd_pos = TvInstallation.from_string(v_rtg_snd_pos_str, default=TvInstallation.BUS)
+    bus_uuid = bus_xml.get('bus_uuid', "")
+    bus = Bus(idx=idx, name=bus_name, reference=src_ref, v_rtg=v_rtg, v_rtg_snd=v_rtg_snd,
+              v_rtg_snd_pos=v_rtg_snd_pos, bus_uuid=bus_uuid)
+    bus.acv_chn = acv_branch_parser(bus_xml.find('scl:ACVChn', ns))
+    for chn in bus_xml.findall('scl:AnaChn', ns):
+        bus.analog_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
+    for chn in bus_xml.findall('scl:StaChn', ns):
+        bus.digital_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
+    return bus
+
+
+def acc_branch_parser(branch_xml) -> ACCBranch:
+    """解析电流分支"""
+    return ACCBranch(idx=branch_xml.get('bran_idx', ""),
+                     ia_idx=branch_xml.get('ia_idx', ""),
+                     ib_idx=branch_xml.get('ib_idx', ""),
+                     ic_idx=branch_xml.get('ic_idx', ""),
+                     in_idx=branch_xml.get('in_idx', ""),
+                     dir=CtDirection.from_string(branch_xml.get('dir', ""), default=CtDirection.POS))
+
+
+def line_parser(line_xml, ns) -> Line:
+    """解析线路"""
+    idx = line_xml.get('idx', 1)
+    line_name = line_xml.get('line_name', "")
+    bus_id = line_xml.get('bus_ID', "")
+    src_ref = line_xml.get('srcRef', "")
+    v_rtg = line_xml.get('VRtg', "")
+    a_rtg = line_xml.get('ARtg', "")
+    a_rtg_snd = line_xml.get('ARtgSnd', "")
+    line_len = line_xml.get('LinLen', 0.0)
+    bran_num = BranNum.from_string(line_xml.get('bran_num', 1), default=BranNum.B1)
+    line_uuid = line_xml.get('line_uuid', "")
+    line = Line(idx=idx, name=line_name, bus_idx=bus_id, reference=src_ref, v_rtg=v_rtg, a_rtg=a_rtg,
+                a_rtg_snd=a_rtg_snd, lin_len=line_len, bran_num=bran_num, line_uuid=line_uuid)
+    rx = line_xml.find('scl:RX', ns)
+    line.rx = RX(r1=rx.get('r1', 0.0), x1=rx.get('x1', 0.0), r0=rx.get('r0', 0.0), x0=rx.get('x0', 0.0))
+    cg = line_xml.find('scl:CG', ns)
+    line.cg = CG(c0=cg.get('c0', 0.0), c1=cg.get('c1', 0.0), g0=cg.get('g0', 0.0), g1=cg.get('g1', 0.0))
+    mr = line_xml.find('scl:MR', ns)
+    line.mr = MR(idx=mr.get('idx', 1), mr0=mr.get('mr0', 0.0), mx0=mr.get('mx0', 0.0))
+    for acc in line_xml.findall('scl:ACC_Bran', ns):
+        line.acc_bran.append(acc_branch_parser(acc))
+    for chn in line_xml.findall('scl:AnaChn', ns):
+        line.ana_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
+    for chn in line_xml.findall('scl:StaChn', ns):
+        line.sta_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
+    return line
+
+
+def transformer_parser(transformer_xml, ns) -> Transformer:
+    idx = transformer_xml.get('idx', 1)
+    tran_name = transformer_xml.get('trm_name', "")
+    src_ref = transformer_xml.get('srcRef', "")
+    pwr_rtg = transformer_xml.get('pwrRtg', "")
+    tranformer_uuid = transformer_xml.get('transformer_uuid', "")
+    tran = Transformer(idx=idx, name=tran_name, reference=src_ref, pwr_rtg=pwr_rtg, transformer_uuid=tranformer_uuid)
+    for tw in transformer_xml.findall('scl:TransformerWinding', ns):
+        location = TransWindLocation.from_string(tw.get('location', ""), default=TransWindLocation.HIGH)
+        src_ref = tw.get('srcRef', "")
+        v_rtg = tw.get('VRtg', "")
+        a_rtg = tw.get('ARtg', "")
+        bran_num = tw.get('bran_num', 1)
+        wg = WG(angle=tw.get('angle', 0), wgroup=WGFlag.from_string(tw.get('wgroup', ""), default=WGFlag.Y))
+        bus_id = tw.get('bus_ID', "")
+        tfw = TransformerWinding(location=location, reference=src_ref, v_rtg=v_rtg, a_rtg=a_rtg,
+                                 bran_num=bran_num, wg=wg, bus_id=bus_id)
+        acv_chn = tw.find('scl:ACVChn', ns)
+        tfw.acv_chn = acv_branch_parser(acv_chn)
+        for chn in tw.findall('scl:ACC_Bran', ns):
+            tfw.acc_bran.append(acc_branch_parser(chn))
+        tran.transWinds.append(tfw)
+    return tran
+
+
+def dmf_parser(_file_path) -> DMF:
+    _dmf = DMF()
+    tree = ET.parse(_file_path)
     root = tree.getroot()
     ns = {
         'scl': 'http://www.iec.ch/61850/2003/SCL'
     }
-    dmf.station_name = root.get('station_name', "变电站")
-    dmf.version = root.get('version', 1.0)
-    dmf.reference = root.get('reference', 0)
-    dmf.rec_dev_name = root.get('rec_dev_name', "录波器")
+    _dmf.station_name = root.get('station_name', "变电站")
+    _dmf.version = root.get('version', 1.0)
+    _dmf.reference = root.get('reference', 0)
+    _dmf.rec_dev_name = root.get('rec_dev_name', "录波器")
 
     for channel in root.findall('scl:AnalogChannel', ns):
-        idx_cfg = channel.get('idx_cfg', None)
-        idx_org = channel.get('idx_org', "")
-        type = channel.get('type', None)
-        flag = channel.get('flag', None)
-        freq = channel.get('freq', 50)
-        au = channel.get('au', 1)
-        bu = channel.get('bu', 0)
-        sIUnit = channel.get('sIUnit', None)
-        multiplier = channel.get('multiplier', "")
-        primary = channel.get('primary', 1)
-        secondary = channel.get('secondary', 1)
-        ps = channel.get('ps', "")
-        ph = channel.get('ph', "")
-        p_min = channel.get('p_min', None)
-        p_max = channel.get('p_max', None)
-        s_min = channel.get('s_min', None)
-        s_max = channel.get('s_max', None)
-        ac = AnalogChannel(idx_cfg=idx_cfg,
-                           idx_org=idx_org,
-                           type=type,
-                           flag=flag,
-                           freq=freq,
-                           au=au,
-                           bu=bu,
-                           s_i_unit=sIUnit,
-                           multiplier=Multiplier.from_string(multiplier, default=Multiplier.N),
-                           primary=primary,
-                           secondary=secondary,
-                           ps=PsType.from_string(ps, default=PsType.S),
-                           phase=ph,
-                           p_max=p_max,
-                           p_min=p_min,
-                           s_max=s_max,
-                           s_min=s_min)
-
-        dmf.analog_channels.append(ac)
+        _dmf.analog_channels.append(analog_channel_parser(channel))
 
     for channel in root.findall('scl:StatusChannel', ns):
-        idx_cfg = channel.get('idx_cfg', None)
-        idx_org = channel.get('idx_org', "")
-        signal_type_str = channel.get('type', None)
-        signal_type = SignalType.from_string(signal_type_str, default=SignalType.RELAY)
-        flag = channel.get('flag', ChannelFlag.GENERAL)
-        if signal_type is SignalType.RELAY:
-            flag = RelayFlag.from_string(flag, default=RelayFlag.TR)
-        elif signal_type is SignalType.BREAKER:
-            flag = BreakerFlag.from_string(flag, default=BreakerFlag.HWJ)
-        elif signal_type is SignalType.WARNING:
-            flag = WarningFlag.from_string(flag, default=WarningFlag.WARN_COMM)
-        else:
-            flag = ChannelFlag.GENERAL
-        contact = channel.get('contact', None)
-        srcRef = channel.get('srcRef', "")
-
-        sc = StatusChannel(idx_cfg=idx_cfg,
-                           idx_org=idx_org,
-                           type=signal_type,
-                           flag=flag,
-                           contact=Contact.from_string(contact, default=Contact.NORMALLY_OPEN),
-                           reference=srcRef)
-        dmf.status_channels.append(sc)
+        _dmf.status_channels.append(status_channel_parser(channel))
 
     for bus in root.findall('scl:Bus', ns):
-        idx = bus.get('idx', 1)
-        bus_name = bus.get('bus_name', "")
-        src_ref = bus.get('srcRef', "")
-        v_rtg = bus.get('VRtg', None)
-        v_rtg_snd = bus.get('VRtgSnd', None)
-        v_rtg_snd_pos_str = bus.get('VRtgSnd_Pos', "")
-        v_rtg_snd_pos = TvInstallation.from_string(v_rtg_snd_pos_str, default=TvInstallation.BUS)
-        bus_uuid = bus.get('bus_uuid', "")
-        b = Bus(idx=idx, name=bus_name, reference=src_ref, v_rtg=v_rtg, v_rtg_snd=v_rtg_snd,
-                v_rtg_snd_pos=v_rtg_snd_pos, bus_uuid=bus_uuid)
-        acv_chn = bus.find('scl:ACVChn', ns)
-        b.acv_chn = ACVBranch(ua_idx=acv_chn.get('ua_idx', ""),
-                              ub_idx=acv_chn.get('ub_idx', ""),
-                              uc_idx=acv_chn.get('uc_idx', ""),
-                              un_idx=acv_chn.get('un_idx', ""),
-                              ul_idx=acv_chn.get('ul_idx', ""))
-        for chn in bus.findall('scl:AnaChn', ns):
-            b.analog_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
-        for chn in bus.findall('scl:StaChn', ns):
-            b.digital_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
-        dmf.buses.append(b)
+        _dmf.buses.append(bus_parser(bus, ns))
 
     for line in root.findall('scl:Line', ns):
-        idx = line.get('idx', 1)
-        line_name = line.get('line_name', "")
-        bus_id = line.get('bus_ID', "")
-        src_ref = line.get('srcRef', "")
-        v_rtg = line.get('VRtg', "")
-        a_rtg = line.get('ARtg', "")
-        a_rtg_snd = line.get('ARtgSnd', "")
-        line_len = line.get('LinLen', 0.0)
-        bran_num = BranNum.from_string(line.get('bran_num', 1), default=BranNum.B1)
-        line_uuid = line.get('line_uuid', "")
-        l = Line(idx=idx, name=line_name, bus_idx=bus_id, reference=src_ref, v_rtg=v_rtg, a_rtg=a_rtg,
-                 a_rtg_snd=a_rtg_snd, lin_len=line_len, bran_num=bran_num, line_uuid=line_uuid)
-        rx = line.find('scl:RX', ns)
-        l.rx = RX(r1=rx.get('r1', 0.0),
-                  x1=rx.get('x1', 0.0),
-                  r0=rx.get('r0', 0.0),
-                  x0=rx.get('x0', 0.0))
-        cg = line.find('scl:CG', ns)
-        l.cg = CG(c0=cg.get('c0', 0.0),
-                  c1=cg.get('c1', 0.0),
-                  g0=cg.get('g0', 0.0),
-                  g1=cg.get('g1', 0.0))
-        mr = line.find('scl:MR', ns)
-        l.mr = MR(idx=mr.get('idx', 1),
-                  mr0=mr.get('mr0', 0.0),
-                  mx0=mr.get('mx0', 0.0))
-        for acc in line.findall('scl:ACC_Bran', ns):
-            l.acc_bran.append(ACCBranch(idx=acc.get('idx', 1),
-                                        ia_idx=acc.get('ia_idx', ""),
-                                        ib_idx=acc.get('ib_idx', ""),
-                                        ic_idx=acc.get('ic_idx', ""),
-                                        in_idx=acc.get('in_idx', ""),
-                                        dir=CtDirection.from_string(acc.get('dir', ""), default=CtDirection.POS)))
-        for chn in line.findall('scl:AnaChn', ns):
-            l.ana_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
-        for chn in line.findall('scl:StaChn', ns):
-            l.sta_chn.append(ChannelIdx(idx_cfg=chn.get('idx_cfg', "")))
-        dmf.lines.append(l)
+        _dmf.lines.append(line_parser(line, ns))
 
     for tran in root.findall('scl:Transformer', ns):
-        idx = tran.get('idx', 1)
-        tran_name = tran.get('trm_name', "")
-        src_ref = tran.get('srcRef', "")
-        pwr_rtg = tran.get('pwrRtg', "")
-        tranformer_uuid = tran.get('transformer_uuid', "")
-        t = Transformer(idx=idx, name=tran_name, reference=src_ref, pwr_rtg=pwr_rtg,
-                        transformer_uuid=tranformer_uuid)
-        for tw in tran.findall('scl:TransformerWinding', ns):
-            location = TransWindLocation.from_string(tw.get('location', ""), default=TransWindLocation.HIGH)
-            src_ref = tw.get('srcRef', "")
-            v_rtg = tw.get('VRtg', "")
-            a_rtg = tw.get('ARtg', "")
-            bran_num = tw.get('bran_num', 1)
-            wg = WG(angle=tw.get('angle', 0), wgroup=WGFlag.from_string(tw.get('wgroup', ""), default=WGFlag.Y))
-            bus_id = tw.get('bus_ID', "")
-            tfw = TransformerWinding(location=location, reference=src_ref, v_rtg=v_rtg, a_rtg=a_rtg,
-                                     bran_num=bran_num, wg=wg, bus_id=bus_id)
-            acv_chn = tw.find('scl:ACVChn', ns)
-            tfw.acv_chn = ACVBranch(ua_idx=acv_chn.get('ua_idx', ""),
-                                    ub_idx=acv_chn.get('ub_idx', ""),
-                                    uc_idx=acv_chn.get('uc_idx', ""),
-                                    un_idx=acv_chn.get('un_idx', ""),
-                                    ul_idx=acv_chn.get('ul_idx', ""))
-            for chn in tw.findall('scl:ACC_Bran', ns):
-                tfw.acc_bran.append(ACCBranch(idx=chn.get('idx', 1),
-                                              ia_idx=chn.get('ia_idx', ""),
-                                              ib_idx=chn.get('ib_idx', ""),
-                                              ic_idx=chn.get('ic_idx', ""),
-                                              in_idx=chn.get('in_idx', ""),
-                                              dir=CtDirection.from_string(chn.get('dir', ""), default=CtDirection.POS)))
-            t.transWinds.append(tfw)
-        dmf.transformers.append(t)
-    return dmf
+        _dmf.transformers.append(transformer_parser(tran, ns))
+    return _dmf
 
 
 if __name__ == '__main__':
