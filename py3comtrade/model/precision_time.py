dc47437916748c9de938b31e230ca69de343adb9
张松贵 <82018238@qq.com>
2025-06-29 23:25:46 +0800

    修复个别录波文件月和日格式错误的情况

--- py3comtrade/model/precision_time.py
+++ py3comtrade/model/precision_time.py
@@ -16,6 +16,7 @@ from pydantic import BaseModel, Field
 
 time_formats = [
     "%d/%m/%Y,%H:%M:%S.%f",  # 四位年
+    "%m/%d/%Y,%H:%M:%S.%f",  # 四位年
     "%m/%d/%y,%H:%M:%S.%f",  # 两位年
     "%Y-%m-%d %H:%M:%S",  # 另一种常见格式
 ]
