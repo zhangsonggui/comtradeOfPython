66ec429b1e4d1f1dfce155997c38d5175695c467
张松贵 <82018238@qq.com>
2025-08-14 16:00:36 +0800

    降低numpy版本，满足python3.10版本

--- tests/test_reader/test_dat_reader.py
+++ tests/test_reader/test_dat_reader.py
@@ -11,8 +11,8 @@ from py3comtrade.reader.data_reader import data_reader
 class TestDataReader(unittest.TestCase):
 
     def setUp(self):
-        cfg_name = r'../dat/xtz._cfg'
-        dat_name = r'../dat/xtz.dat'
+        cfg_name = r'../data/xtz.cfg'
+        dat_name = r'../data/xtz.dat'
         self.cfg = config_reader(cfg_name)
         self.dat = data_reader(dat_name, self.cfg.sample)
 
